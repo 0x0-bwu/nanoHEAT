bc771b2210101b4b58d9e35df24e6694526005ad
0x0-bwu <350240702.qq.com>
2025-02-07 23:40:05 +0800

    impl: layout polygon merger

---
+++ src/model/utils/NSModelLayoutPolygonMerger.cpp
@@ -0,0 +1,8 @@
+#include "NSModelLayoutPolygonMerger.h"
+
+#include "generic/geometry/PolygonMerge.hpp"
+#include <nano/core/package>
+
+namespace nano::heat::model::utils {
+
+} // namespace nano::heat::model::utils
---
+++ src/model/utils/NSModelLayoutPolygonMerger.h
@@ -0,0 +1,12 @@
+#pragma once
+#include "basic/NSHeatCommon.hpp"
+#include <nano/fwd>
+namespace nano::heat::model::utils {
+
+class LayoutPolygonMerger
+{
+public:
+    
+};
+
+} // namespace nano::heat::model::utils
