71b7440f26612198e4fad89c60ebca0d3f4cdadb
0x0-bwu <350240702.qq.com>
2025-02-11 23:35:55 +0800

    impl: prism stackup layer model query

--- src/model/utils/NSModelPrismStackupThermalQuery.h
+++ src/model/utils/NSModelPrismStackupThermalQuery.h
@@ -1,30 +1,29 @@
 #pragma once
 #include <boost/geometry/index/rtree.hpp>
-#include "model/NSModelPrismThermal.h"
+#include "model/NSModelPrismStackupThermal.h"
 
-namespace nano::heat::model::utils { 
+namespace nano::heat::model::utils {
 
-class PrismThermalModelQuery
+class PrismStackupThermalModelQuery
 {
 public:
-    using RtVal = Pair<NCoord2D, Index>;
+    using RtVal = std::pair<NCoord2D, size_t>;
     using Rtree = boost::geometry::index::rtree<RtVal, boost::geometry::index::rstar<8>>;
-    using Model = nano::heat::model::PrismThermalModel;
-    explicit PrismThermalModelQuery(CRef<Model> model, bool lazyBuild = true);
-    virtual ~PrismThermalModelQuery() = default;
-    void SearchPrismInstances(const NBox2D & area, Vec<RtVal> & results) const;
+    using Model = nano::heat::model::PrismStackupThermalModel;
+    explicit PrismStackupThermalModelQuery(CRef<Model> model, bool lazyBuild = false);
+    virtual ~PrismStackupThermalModelQuery() = default;
+    
+    void IntersectsPrismInstances(Index layer, Index pid, Vec<RtVal> & results) const;
     void SearchPrismInstances(Index layer, const NBox2D & area, Vec<RtVal> & results) const;
     void SearchNearestPrismInstances(Index layer, const NCoord2D & pt, size_t k, Vec<RtVal> & results) const;
     Index NearestLayer(Float height) const;
 
 protected:
-    CPtr<Rtree> BuildIndexTree() const;
     CPtr<Rtree> BuildLayerIndexTree(Index layer) const;
 protected:
     CRef<Model> m_model;
 
     mutable std::mutex m_mutex;
-    mutable UPtr<Rtree> m_rtree{nullptr};
     mutable HashMap<Index, SPtr<Rtree>> m_lyrRtrees;
 };
 
